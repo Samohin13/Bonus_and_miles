10cd74a9540617e6ccfd62ae74a8db2b27436e4c
Samohin Sergei <Bravochief21@gmail.com>
2024-07-03 13:31:31 +0500

    changer commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,10 @@
-import org.w3c.dom.ls.LSOutput;
-
-
 public class Main {
-
-
     public static void main(String[] args) {
         int amount = 20_000; // стоимость билета
-        boolean ticket = true;
-
-        int percent;
-        if (ticket) {
-            percent = 5;
-        } else {
-            percent = 0;
-        }
+        int percent = 5; // процент с покупки билета
 
         int bonus = amount * percent / 100;
+
         System.out.println("Стоимость билета:" + amount + " рублей ");
         System.out.println("Накопленные мили:" + bonus);
     }
